eebd78b7a0f4dfa98ad0ddaece28a27b2a10f87b
fish fish <yuzhongrong2018@gmail.com>
2025-07-19 14:45:36 +0000

    点击链接钱包没有反应 并提醒警告： useStandardWalletAdapters.ts:49 Phantom was registered

--- src/components/wallet-provider.tsx
+++ src/components/wallet-provider.tsx
@@ -3,9 +3,6 @@
 import React, { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import {
-    PhantomWalletAdapter,
-} from '@solana/wallet-adapter-phantom';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 
@@ -18,9 +15,8 @@ export const WalletContextProvider = ({
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
     const wallets = useMemo(
-        () => [
-            new PhantomWalletAdapter(),
-        ],
+        () => [],
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         [network]
     );
 
